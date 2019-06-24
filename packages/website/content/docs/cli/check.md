062143e0cf9a50b566c5dcd6feaaffedb11be3ae
Ken <kchau@microsoft.com>
2019-06-24 13:30:33 -0700

    funy css for pushing footer down

--- packages/website/content/docs/cli/check.md
+++ packages/website/content/docs/cli/check.md
@@ -3,3 +3,31 @@ title: 'check'
 tags: cli
 category: doc
 ---
+
+It is useful to enforce that [change files](../change-files) are checked in for each PR before they enter the target branch. In this way, all changes are captured and would affect semver appropriately. To check to make sure all changes are captured in change files, simply run:
+
+```bash
+beachball check
+```
+
+#### Where Should Check Be Run?
+
+###### As a step in the PR review gate
+
+- Travis CI:
+
+```yaml
+language: node_js
+node_js:
+  - '10'
+script:
+  - yarn
+  - yarn check
+  - yarn build
+  - yarn test
+```
+
+###### As git hook (optional, but good for dev experience)
+
+For a reference of git hooks, take a look at this documentation
+https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks. It is recommended to place this hook as a pre-push. There are ways to hook this up there,
--- packages/website/src/pages/index.jsx
+++ packages/website/src/pages/index.jsx
@@ -1,7 +1,6 @@
 import { graphql } from 'gatsby'
 import React from 'react'
 import Helmet from 'react-helmet'
-import { ThemeContext } from 'styled-components'
 import styled from 'styled-components'
 import {
   FaSync,
@@ -21,88 +20,93 @@ import beachBallSvg from '../assets/beach-ball.svg'
 const Index = props => {
   return (
     <Layout location={props.location}>
-      <div className="index-container">
+      <IndexContainer>
         <Helmet title={config.siteTitle} />
-        <main>
-          <IndexHeadContainer>
-            <Navigation />
-            <Hero>
-              <LogoRow>
-                <BeachBallLogo src={beachBallSvg} />
-                <h1>{config.siteTitle}</h1>
-              </LogoRow>
-              <h4>{config.siteDescription}</h4>
-              <CtaButton to={'/getting-started'}>Getting Started</CtaButton>
-            </Hero>
-          </IndexHeadContainer>
-          <BodyContainer>
-            <FeatureRow>
-              <Feature>
-                <FaSync size="3rem" />
-                <h4>Synchronized in git and npm</h4>
-                keep your git and npm versions in sync in CI and local workflows
-              </Feature>
-              <Feature>
-                <FaRobot size="3rem" />
-                <h4>Automated Version Bumps</h4>
-                one command line to bump package(s) in your repo with semver
-              </Feature>
-            </FeatureRow>
 
-            <FeatureRow>
-              <Feature>
-                <FaNewspaper size="3rem" />
-                <h4>Generates Changelogs</h4>
-                same command will generate changelogs for your users
-              </Feature>
-              <Feature>
-                <DiGitBranch size="3rem" />
-                <h4>Single or Monorepo</h4>
-                compatible out of the box for single repo or lerna repos
-              </Feature>
-            </FeatureRow>
-            <FeatureRow>
-              <Feature>
-                <FaCheckDouble size="3rem" />
-                <h4>Pre-Publish Validation Checks</h4>
-                double and triple check git repo and npm registry before publish
-              </Feature>
-              <Feature>
-                <FaTerminal size="3rem" />
-                <h4>Zero Config Versioning</h4>
-                no config is required to get started, do more in one line
-              </Feature>
-            </FeatureRow>
-          </BodyContainer>
-          <FooterContainer>
-            <div className="contributors">
-              <div>
-                Icons made by{' '}
-                <a href="https://www.freepik.com/" title="Freepik">
-                  Freepik
-                </a>{' '}
-                from{' '}
-                <a href="https://www.flaticon.com/" title="Flaticon">
-                  www.flaticon.com
-                </a>{' '}
-                is licensed by{' '}
-                <a
-                  href="http://creativecommons.org/licenses/by/3.0/"
-                  title="Creative Commons BY 3.0"
-                  target="_blank"
-                >
-                  CC 3.0 BY
-                </a>
-              </div>
+        <IndexHeadContainer>
+          <Navigation />
+          <Hero>
+            <LogoRow>
+              <BeachBallLogo src={beachBallSvg} />
+              <h1>{config.siteTitle}</h1>
+            </LogoRow>
+            <h4>{config.siteDescription}</h4>
+            <CtaButton to={'/getting-started'}>Getting Started</CtaButton>
+          </Hero>
+        </IndexHeadContainer>
+        <BodyContainer>
+          <FeatureRow>
+            <Feature>
+              <FaSync size="3rem" />
+              <h4>Synchronized in git and npm</h4>
+              keep your git and npm versions in sync in CI and local workflows
+            </Feature>
+            <Feature>
+              <FaRobot size="3rem" />
+              <h4>Automated Version Bumps</h4>
+              one command line to bump package(s) in your repo with semver
+            </Feature>
+          </FeatureRow>
+
+          <FeatureRow>
+            <Feature>
+              <FaNewspaper size="3rem" />
+              <h4>Generates Changelogs</h4>
+              same command will generate changelogs for your users
+            </Feature>
+            <Feature>
+              <DiGitBranch size="3rem" />
+              <h4>Single or Monorepo</h4>
+              compatible out of the box for single repo or lerna repos
+            </Feature>
+          </FeatureRow>
+          <FeatureRow>
+            <Feature>
+              <FaCheckDouble size="3rem" />
+              <h4>Pre-Publish Validation Checks</h4>
+              double and triple check git repo and npm registry before publish
+            </Feature>
+            <Feature>
+              <FaTerminal size="3rem" />
+              <h4>Zero Config Versioning</h4>
+              no config is required to get started, do more in one line
+            </Feature>
+          </FeatureRow>
+        </BodyContainer>
+        <FooterContainer>
+          <div className="contributors">
+            <div>
+              Icons made by{' '}
+              <a href="https://www.freepik.com/" title="Freepik">
+                Freepik
+              </a>{' '}
+              from{' '}
+              <a href="https://www.flaticon.com/" title="Flaticon">
+                www.flaticon.com
+              </a>{' '}
+              is licensed by{' '}
+              <a
+                href="http://creativecommons.org/licenses/by/3.0/"
+                title="Creative Commons BY 3.0"
+                target="_blank"
+              >
+                CC 3.0 BY
+              </a>
             </div>
-          </FooterContainer>
-        </main>
-      </div>
+          </div>
+        </FooterContainer>
+      </IndexContainer>
     </Layout>
   )
 }
 
 export default Index
+
+const IndexContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
 
 const FeatureRow = styled.div`
   display: flex;
@@ -160,20 +164,24 @@ const BodyContainer = styled.div`
   max-width: ${props => props.theme.contentWidthLaptop};
   margin: 0 auto;
 
+  flex-grow: 1;
+
   .contributors {
     margin: 100px auto 0;
   }
+
   .contributors a {
     font-size: 1rem;
   }
+
+  margin-bottom: 100px;
 `
 
-const FooterContainer = styled.div`
+const FooterContainer = styled.footer`
   background: ${props => props.theme.lightGrey};
 
-  bottom: 0;
-  width: 100%;
-  height: 100px; /* Height of the footer */
+  height: 100px;
+
   padding-top: 30px;
   display: flex;
   justify-content: center;
