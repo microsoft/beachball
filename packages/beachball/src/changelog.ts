c4997de03aca9b1219fba3300dadf13a7d51fc2b
Ken <kchau@microsoft.com>
2019-08-22 13:38:53 -0700

    fixing changelog formatting

--- packages/beachball/src/changelog.ts
+++ packages/beachball/src/changelog.ts
@@ -71,7 +71,12 @@ export function writeChangelog(packageInfos: { [pkg: string]: PackageInfo }, cwd
   Object.keys(changelogs).forEach(pkg => {
     const packagePath = path.dirname(packageInfos[pkg].packageJsonPath);
 
-    if (changelogs[pkg].comments.major || changelogs[pkg].comments.minor || changelogs[pkg].comments.patch || changelogs[pkg].comments.prerelease) {
+    if (
+      changelogs[pkg].comments.major ||
+      changelogs[pkg].comments.minor ||
+      changelogs[pkg].comments.patch ||
+      changelogs[pkg].comments.prerelease
+    ) {
       const changelogFile = path.join(packagePath, 'CHANGELOG.md');
       const previousContent = fs.existsSync(changelogFile) ? fs.readFileSync(changelogFile).toString() : '';
       const nextContent = renderChangelog(previousContent, changelogs[pkg]);
@@ -125,16 +130,16 @@ function renderPackageChangelog(changelog: PackageChangelog) {
     `\n## ${changelog.version}\n` +
     `${changelog.date.toUTCString()}\n` +
     (changelog.comments.major
-      ? '\n### Major\n\n' + changelog.comments.major.map(change => `- ${change.comment} (${change.author})\n`)
+      ? '\n### Major\n\n' + changelog.comments.major.map(change => `- ${change.comment} (${change.author})`).join('\n')
       : '') +
     (changelog.comments.minor
-      ? '\n### Minor changes\n\n' + changelog.comments.minor.map(change => `- ${change.comment} (${change.author})\n`)
+      ? '\n### Minor changes\n\n' + changelog.comments.minor.map(change => `- ${change.comment} (${change.author})`).join('\n')
       : '') +
     (changelog.comments.patch
-      ? '\n### Patches\n\n' + changelog.comments.patch.map(change => `- ${change.comment} (${change.author})\n`)
+      ? '\n### Patches\n\n' + changelog.comments.patch.map(change => `- ${change.comment} (${change.author})`).join('\n')
       : '') +
-      (changelog.comments.prerelease
-        ? '\n### Changes\n\n' + changelog.comments.prerelease.map(change => `- ${change.comment} (${change.author})\n`)
-        : '')
+    (changelog.comments.prerelease
+      ? '\n### Changes\n\n' + changelog.comments.prerelease.map(change => `- ${change.comment} (${change.author})`).join('\n')
+      : '')
   );
 }
