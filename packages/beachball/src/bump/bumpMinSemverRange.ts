1d6abc44cea96fafbfbbafa7f6792d97ee2c545d
Ken Chau <kchau@microsoft.com>
2020-03-05 08:54:30 -0800

    adding some bumpminrange test and add support for * as range

--- packages/beachball/src/bump/bumpMinSemverRange.ts
+++ packages/beachball/src/bump/bumpMinSemverRange.ts
@@ -1,11 +1,16 @@
 import semver from 'semver';
 
 export function bumpMinSemverRange(minVersion: string, semverRange: string) {
+  if (semverRange === '*') {
+    return semverRange;
+  }
+
   if (semverRange.startsWith('~') || semverRange.startsWith('^')) {
     // ~1.0.0
     // ^1.0.0
     return semverRange[0] + minVersion;
   } else if (semverRange.includes('>')) {
+    // Less frequently used, but we treat any of these kinds of ranges to be within a minor band for now: more complex understand of the semver range utility is needed to do more
     // >=1.0.0 <2.0.0
     return `>=${minVersion} <${semver.inc(minVersion, 'major')}`;
   } else if (semverRange.includes(' - ')) {
