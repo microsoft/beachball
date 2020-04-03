7060517551d5e48364e8cb457150f9d25a53ac5c
Elizabeth Craig <elcraig@microsoft.com>
2020-04-02 18:07:29 -0700

    comment

--- packages/beachball/src/index.ts
+++ packages/beachball/src/index.ts
@@ -1,4 +1,5 @@
-// Public API of Beachball
+// Public types of Beachball used in config files
+// (the package does not have a callable public API since it's meant to be invoked via command line)
 export { RepoOptions, PackageOptions, VersionGroupOptions } from './types/BeachballOptions';
 export { ChangeFilePromptOptions } from './types/ChangeFilePrompt';
 export { ChangeType } from './types/ChangeInfo';
