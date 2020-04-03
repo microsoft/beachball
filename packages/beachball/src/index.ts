59ff68552852c27248e9edf37d1c2935b32abd62
Elizabeth Craig <elcraig@microsoft.com>
2020-04-02 18:05:04 -0700

    Export public-facing types from root

---
+++ packages/beachball/src/index.ts
@@ -0,0 +1,10 @@
+// Public API of Beachball
+export { RepoOptions, PackageOptions, VersionGroupOptions } from './types/BeachballOptions';
+export { ChangeFilePromptOptions } from './types/ChangeFilePrompt';
+export { ChangeType } from './types/ChangeInfo';
+export {
+  ChangelogOptions,
+  ChangelogGroupOptions,
+  PackageChangelogRenderInfo,
+  ChangelogRenderers,
+} from './types/ChangelogOptions';
