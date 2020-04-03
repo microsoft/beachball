af9383dd7c5f57a43c2a873aa72267a5721fe9ac
Elizabeth Craig <elcraig@microsoft.com>
2020-04-02 18:10:03 -0700

    make type less restrictive

--- packages/beachball/src/index.ts
+++ packages/beachball/src/index.ts
@@ -1,6 +1,10 @@
 // Public types of Beachball used in config files
 // (the package does not have a callable public API since it's meant to be invoked via command line)
-export { RepoOptions, PackageOptions, VersionGroupOptions } from './types/BeachballOptions';
+import { RepoOptions, PackageOptions } from './types/BeachballOptions';
+
+export type BeachballConfig = Partial<RepoOptions> & Partial<PackageOptions>;
+
+export { VersionGroupOptions } from './types/BeachballOptions';
 export { ChangeFilePromptOptions } from './types/ChangeFilePrompt';
 export { ChangeType } from './types/ChangeInfo';
 export {
