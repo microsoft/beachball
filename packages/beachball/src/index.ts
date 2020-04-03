315b9e58981b7d0dca12cb8370bd7fd9880a975e
Elizabeth Craig <elcraig@microsoft.com>
2020-04-03 08:56:11 -0700

    Export public-facing types from root (#306)

---
+++ packages/beachball/src/index.ts
@@ -0,0 +1,15 @@
+// Public types of Beachball used in config files
+// (the package does not have a callable public API since it's meant to be invoked via command line)
+import { RepoOptions, PackageOptions } from './types/BeachballOptions';
+
+export type BeachballConfig = Partial<RepoOptions> & Partial<PackageOptions>;
+
+export { VersionGroupOptions } from './types/BeachballOptions';
+export { ChangeFilePromptOptions } from './types/ChangeFilePrompt';
+export { ChangeType } from './types/ChangeInfo';
+export {
+  ChangelogOptions,
+  ChangelogGroupOptions,
+  PackageChangelogRenderInfo,
+  ChangelogRenderers,
+} from './types/ChangelogOptions';
