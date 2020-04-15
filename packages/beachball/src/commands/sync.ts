672706a02f46815741ae83329b0b1d7fce5c3b9e
Xu Gao <xgao@microsoft.com>
2020-04-15 16:16:06 -0700

    fix sync

--- packages/beachball/src/commands/sync.ts
+++ packages/beachball/src/commands/sync.ts
@@ -1,4 +1,5 @@
 import { BeachballOptions } from '../types/BeachballOptions';
+import { getScopedPackages } from '../monorepo/getScopedPackages';
 import { getPackageInfos } from '../monorepo/getPackageInfos';
 import { npm } from '../packageManager/npm';
 import semver from 'semver';
@@ -6,8 +7,13 @@ import fs from 'fs-extra';
 
 export async function sync(options: BeachballOptions) {
   const packageInfos = getPackageInfos(options.path);
+  const scopedPackages = new Set(getScopedPackages(options));
 
   for (const [pkg, info] of Object.entries(packageInfos)) {
+    if (!scopedPackages.has(pkg) || info.private) {
+      continue;
+    }
+
     const npmArgs = ['view', pkg, 'version'];
     if (options.registry) {
       npmArgs.push('--registry');
@@ -16,8 +22,10 @@ export async function sync(options: BeachballOptions) {
     const result = npm(npmArgs);
     const publishedVersion = result.stdout;
 
-    if (semver.lt(info.version, publishedVersion)) {
-      console.log(`There is a newer version of "${pkg}@${info.version}". Syncing to the published version ${publishedVersion}`)
+    if (publishedVersion && semver.lt(info.version, publishedVersion)) {
+      console.log(
+        `There is a newer version of "${pkg}@${info.version}". Syncing to the published version ${publishedVersion}`
+      );
 
       const packageJson = fs.readJsonSync(info.packageJsonPath);
       packageJson.version = publishedVersion;
