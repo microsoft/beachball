952278a367f879cb1c33c297efbb4e0d1132bc8e
Elizabeth Craig <elcraig@microsoft.com>
2020-04-22 10:00:29 -0700

    Sync should limit to current major version

--- packages/beachball/src/commands/sync.ts
+++ packages/beachball/src/commands/sync.ts
@@ -14,17 +14,28 @@ export async function sync(options: BeachballOptions) {
       continue;
     }
 
-    const npmArgs = ['view', pkg, 'version'];
+    // Get the latest of *this major version* of the package.
+    // To do this we have to get all versions within this major version then take the latest.
+    const npmArgs = ['view', `${pkg}@${semver.major(info.version)}`, 'version', '--json'];
     if (options.registry) {
       npmArgs.push('--registry');
       npmArgs.push(options.registry);
     }
     const result = npm(npmArgs);
-    const publishedVersion = result.stdout;
+    let publishedVersion = '';
+
+    try {
+      // The returned array of versions will already be sorted and not include prerelease because
+      // prerelease versions don't satisfy semver pkg@x.
+      publishedVersion = JSON.parse(result.stdout).slice(-1)[0];
+    } catch (ex) {
+      // Package is not published or possibly some other issue (don't need to warn user)
+    }
 
     if (publishedVersion && semver.lt(info.version, publishedVersion)) {
       console.log(
-        `There is a newer version of "${pkg}@${info.version}". Syncing to the published version ${publishedVersion}`
+        `There is a newer version of "${pkg}" (local version: ${info.version}). ` +
+          `Syncing to the published version ${publishedVersion}.`
       );
 
       const packageJson = fs.readJsonSync(info.packageJsonPath);
