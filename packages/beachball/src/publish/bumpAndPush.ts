8d19fc4f75a6ae68d937cdbfe6abeecc778c4afe
Ken Chau <kchau@microsoft.com>
2020-07-20 13:02:38 -0700

    adds a retry to git push

--- packages/beachball/src/publish/bumpAndPush.ts
+++ packages/beachball/src/publish/bumpAndPush.ts
@@ -6,53 +6,67 @@ import { tagDistTag, tagPackages } from './tagPackages';
 import { mergePublishBranch } from './mergePublishBranch';
 import { displayManualRecovery } from './displayManualRecovery';
 
+const BUMP_PUSH_RETRIES = 5;
+
 export async function bumpAndPush(bumpInfo: BumpInfo, publishBranch: string, options: BeachballOptions) {
   const { path: cwd, branch, tag, message } = options;
   const { remote, remoteBranch } = parseRemoteBranch(branch);
 
-  console.log('Reverting');
-  revertLocalChanges(cwd);
+  let error = '';
+  let completed = false;
+  let tryNumber = 0;
 
-  // pull in latest from origin branch
-  console.log('Fetching from remote');
-  gitFailFast(['fetch', remote], { cwd });
-  const mergeResult = git(['merge', '-X', 'theirs', `${branch}`], { cwd });
-  if (!mergeResult.success) {
-    console.error(`CRITICAL ERROR: pull from ${branch} has failed!`);
-    console.error(mergeResult.stderr);
-    process.exit(1);
+  while (tryNumber < BUMP_PUSH_RETRIES && completed) {
+    tryNumber++;
+    console.log(`Trying to push to git. Attempt ${tryNumber}/${BUMP_PUSH_RETRIES}`);
+    console.log('Reverting');
+    revertLocalChanges(cwd);
+
+    // pull in latest from origin branch
+    console.log('Fetching from remote');
+    gitFailFast(['fetch', remote], { cwd });
+    const mergeResult = git(['merge', '-X', 'theirs', `${branch}`], { cwd });
+    if (!mergeResult.success) {
+      error = `CRITICAL ERROR: pull from ${branch} has failed!\n${mergeResult.stderr}`;
+      continue;
+    }
+
+    // bump the version
+    console.log('Bumping the versions for git push');
+    await performBump(bumpInfo, options);
+
+    // checkin
+    const mergePublishBranchResult = mergePublishBranch(publishBranch, branch, message, cwd);
+    if (!mergePublishBranchResult.success) {
+      error = `CRITICAL ERROR: merging to target has failed!`;
+      continue;
+    }
+
+    // Tag & Push to remote
+    tagPackages(bumpInfo, cwd);
+    if (options.gitTags) {
+      tagDistTag(tag, cwd);
+    }
+
+    console.log(`pushing to ${branch}, running the following command for git push:`);
+    const pushArgs = ['push', '--no-verify', '--follow-tags', '--verbose', remote, `HEAD:${remoteBranch}`];
+    console.log('git ' + pushArgs.join(' '));
+
+    const pushResult = git(pushArgs, { cwd });
+
+    if (!pushResult.success) {
+      error = `CRITICAL ERROR: push to ${branch} has failed!\n${pushResult.stderr}`);
+      continue;
+    } else {
+      console.log(pushResult.stdout.toString());
+      console.log(pushResult.stderr.toString());
+      completed = true;
+    }
   }
 
-  // bump the version
-  console.log('Bumping the versions for git push');
-  await performBump(bumpInfo, options);
-
-  // checkin
-  const mergePublishBranchResult = mergePublishBranch(publishBranch, branch, message, cwd);
-  if (!mergePublishBranchResult.success) {
-    console.error('CRITICAL ERROR: merging to target has failed!');
+  if (error) {
+    console.error(error);
     displayManualRecovery(bumpInfo);
     process.exit(1);
-  }
-
-  // Tag & Push to remote
-  tagPackages(bumpInfo, cwd);
-  if (options.gitTags) {
-    tagDistTag(tag, cwd);
-  }
-
-  console.log(`pushing to ${branch}, running the following command for git push:`);
-  const pushArgs = ['push', '--no-verify', '--follow-tags', '--verbose', remote, `HEAD:${remoteBranch}`];
-  console.log('git ' + pushArgs.join(' '));
-
-  const pushResult = git(pushArgs, { cwd });
-  if (!pushResult.success) {
-    console.error(`CRITICAL ERROR: push to ${branch} has failed!`);
-    console.error(pushResult.stderr);
-    displayManualRecovery(bumpInfo);
-    process.exit(1);
-  } else {
-    console.log(pushResult.stdout.toString());
-    console.log(pushResult.stderr.toString());
   }
 }
