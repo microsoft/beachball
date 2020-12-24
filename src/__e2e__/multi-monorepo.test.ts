2972100b721ceabe8a033cf5fef97ae2e974b1af
Benjamin Weggersen <bewegger@microsoft.com>
2020-12-24 08:44:50 +0100

    Add changed files detection tests

--- src/__e2e__/multi-monorepo.test.ts
+++ src/__e2e__/multi-monorepo.test.ts
@@ -1,6 +1,7 @@
 import fs from 'fs-extra';
 import path from 'path';
 import { RepositoryFactory } from '../fixtures/repository';
+import { getChangedPackages } from '../changefile/getChangedPackages';
 import { writeChangeFiles } from '../changefile/writeChangeFiles';
 import { git } from '../git';
 import { bump } from '../commands/bump';
@@ -8,6 +9,52 @@ import { getPackageInfos } from '../monorepo/getPackageInfos';
 import { BeachballOptions } from '../types/BeachballOptions';
 import { getChangePath } from '../paths';
 import { MultiMonoRepoFactory } from '../fixtures/multi-monorepo';
+
+describe('changed files', () => {
+  let repositoryFactory: RepositoryFactory | undefined;
+
+  afterEach(async () => {
+    if (repositoryFactory) {
+      repositoryFactory.cleanUp();
+      repositoryFactory = undefined;
+    }
+  });
+
+  it('detects changed files in workspace', async () => {
+    repositoryFactory = new MultiMonoRepoFactory();
+    repositoryFactory.create();
+    const repo = repositoryFactory.cloneRepository();
+
+    const repoARoot = path.join(repo.rootPath, 'repo-a');
+    const repoBRoot = path.join(repo.rootPath, 'repo-b');
+
+    const testFilePath = path.join(repoARoot, 'packages/foo/test.js');
+    fs.writeFileSync(testFilePath, '');
+    git(['add', testFilePath], { cwd: repoARoot });
+
+    const changedPackagesA = getChangedPackages({
+      fetch: false,
+      path: repoARoot,
+      branch: 'master',
+    } as BeachballOptions);
+
+    const changedPackagesB = getChangedPackages({
+      fetch: false,
+      path: repoBRoot,
+      branch: 'master',
+    } as BeachballOptions);
+
+    const changedPackagesRoot = getChangedPackages({
+      fetch: false,
+      path: repo.rootPath,
+      branch: 'master',
+    } as BeachballOptions);
+
+    expect(changedPackagesA).toStrictEqual(['foo']);
+    expect(changedPackagesB).toStrictEqual([]);
+    expect(changedPackagesRoot).toStrictEqual(['foo']);
+  });
+});
 
 describe('version bumping', () => {
   let repositoryFactory: RepositoryFactory | undefined;
@@ -25,10 +72,10 @@ describe('version bumping', () => {
     }
   });
 
-  it('bump workspace package', async () => {
+  it('only bumps workspace package', async () => {
     repositoryFactory = new MultiMonoRepoFactory();
-    await repositoryFactory.create();
-    const repo = await repositoryFactory.cloneRepository();
+    repositoryFactory.create();
+    const repo = repositoryFactory.cloneRepository();
 
     const repoARoot = path.join(repo.rootPath, 'repo-a');
     const repoBRoot = path.join(repo.rootPath, 'repo-b');
@@ -46,35 +93,10 @@ describe('version bumping', () => {
       repoARoot
     );
 
-    git(['push', 'origin', 'master'], { cwd: repo.rootPath });
-
-    await bump({ path: repoARoot, bumpDeps: true } as BeachballOptions);
-
-    const packageInfosA = getPackageInfos(repoARoot);
-    expect(packageInfosA['foo'].version).toBe('1.1.0');
-
-    const packageInfosB = getPackageInfos(repoBRoot);
-    expect(packageInfosB['foo'].version).toBe('1.0.0');
-
-    const changeFilesA = getChangeFiles(repoARoot);
-    expect(changeFilesA.length).toBe(0);
-
-    const changeFilesB = getChangeFiles(repoBRoot);
-    expect(changeFilesB.length).toBe(0);
-  });
-
-  it('should not bump out-of-workspace package even if package has change', async () => {
-    repositoryFactory = new MultiMonoRepoFactory();
-    await repositoryFactory.create();
-    const repo = await repositoryFactory.cloneRepository();
-
-    const repoARoot = path.join(repo.rootPath, 'repo-a');
-    const repoBRoot = path.join(repo.rootPath, 'repo-b');
-
     writeChangeFiles(
       {
         foo: {
-          type: 'minor',
+          type: 'major',
           comment: 'test',
           email: 'test@test.com',
           packageName: 'foo',
@@ -89,15 +111,13 @@ describe('version bumping', () => {
     await bump({ path: repoARoot, bumpDeps: true } as BeachballOptions);
 
     const packageInfosA = getPackageInfos(repoARoot);
-    expect(packageInfosA['foo'].version).toBe('1.0.0');
-
     const packageInfosB = getPackageInfos(repoBRoot);
+    expect(packageInfosA['foo'].version).toBe('1.1.0');
     expect(packageInfosB['foo'].version).toBe('1.0.0');
 
     const changeFilesA = getChangeFiles(repoARoot);
-    expect(changeFilesA.length).toBe(0);
-
     const changeFilesB = getChangeFiles(repoBRoot);
+    expect(changeFilesA.length).toBe(0);
     expect(changeFilesB.length).toBe(1);
   });
 });
