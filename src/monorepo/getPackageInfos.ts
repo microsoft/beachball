a312b5c1308f579c8e4b08179c6f4d59cc53a78c
Ken <kchau@microsoft.com>
2021-02-17 12:43:32 -0800

    Change files

--- src/monorepo/getPackageInfos.ts
+++ src/monorepo/getPackageInfos.ts
@@ -1,30 +1,36 @@
 import { findPackageRoot, findProjectRoot } from '../paths';
 import fs from 'fs-extra';
 import path from 'path';
-import { listAllTrackedFiles } from 'workspace-tools';
+import { getWorkspaces } from 'workspace-tools';
 import { PackageInfos } from '../types/PackageInfo';
 import { infoFromPackageJson } from './infoFromPackageJson';
 
 export function getPackageInfos(cwd: string) {
   const root = findProjectRoot(cwd)!;
-  const packageJsonFiles = listAllTrackedFiles(['**/package.json', 'package.json'], root);
   const packageInfos: PackageInfos = {};
 
-  if (packageJsonFiles && packageJsonFiles.length > 0) {
-    packageJsonFiles.forEach(packageJsonPath => {
-      try {
-        const packageJsonFullPath = path.join(root, packageJsonPath);
-        const packageJson = fs.readJSONSync(packageJsonFullPath);
-        packageInfos[packageJson.name] = infoFromPackageJson(packageJson, packageJsonFullPath);
-      } catch (e) {
-        // Pass, the package.json is invalid
-        console.warn(`Invalid package.json file detected ${packageJsonPath}: `, e);
-      }
-    });
-  } else {
-    const packageJsonFullPath = path.join(root, findPackageRoot(cwd)!, 'package.json');
+  try {
+    const workspaceInfo = getWorkspaces(root);
+
+    if (workspaceInfo && workspaceInfo.length > 0) {
+      workspaceInfo.forEach(info => {
+        const { path: packagePath, packageJson } = info;
+        const packageJsonPath = path.join(packagePath, 'package.json');
+
+        try {
+          packageInfos[packageJson.name] = infoFromPackageJson(packageJson, packageJsonPath);
+        } catch (e) {
+          // Pass, the package.json is invalid
+          console.warn(`Invalid package.json file detected ${packageJsonPath}: `, e);
+        }
+      });
+    }
+
+    return packageInfos;
+  } catch (e) {
+    const packageJsonFullPath = path.resolve(findPackageRoot(cwd)!, 'package.json');
     const packageJson = fs.readJSONSync(packageJsonFullPath);
     packageInfos[packageJson.name] = infoFromPackageJson(packageJson, packageJsonFullPath);
+    return packageInfos;
   }
-  return packageInfos;
 }
