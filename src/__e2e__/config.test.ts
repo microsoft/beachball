0bc63fee2681e1ae2a05be5c88f996090b4b5210
Elizabeth Craig <elcraig@microsoft.com>
2021-07-28 14:52:44 -0700

    Update jest and typescript (#561)

--- src/__e2e__/config.test.ts
+++ src/__e2e__/config.test.ts
@@ -1,18 +1,14 @@
 import fs from 'fs-extra';
 import { RepositoryFactory } from '../fixtures/repository';
-import * as process from 'process';
-import { promisify } from 'util';
 import { getOptions } from '../options/getOptions';
-
-const writeFileAsync = promisify(fs.writeFile);
 
 const baseArgv = ['node.exe', 'bin.js'];
 
 describe('config', () => {
   it('uses the branch name defined in beachball.config.js', async () => {
     const repositoryFactory = new RepositoryFactory();
-    await repositoryFactory.create();
-    const repo = await repositoryFactory.cloneRepository();
+    repositoryFactory.create();
+    const repo = repositoryFactory.cloneRepository();
     const config = await inDirectory(repo.root!, async () => {
       await writeConfig('module.exports = { branch: "origin/main" };');
       return getOptions(baseArgv);
@@ -23,21 +19,21 @@ describe('config', () => {
 
 it('--config overrides configuration path', async () => {
   const repositoryFactory = new RepositoryFactory();
-  await repositoryFactory.create();
-  const repo = await repositoryFactory.cloneRepository();
+  repositoryFactory.create();
+  const repo = repositoryFactory.cloneRepository();
   const config = await inDirectory(repo.root!, async () => {
     await writeConfig('module.exports = { branch: "origin/main" };');
-    await writeFileAsync('alternate.config.js', 'module.exports = { branch: "origin/foo" };');
+    await fs.writeFile('alternate.config.js', 'module.exports = { branch: "origin/foo" };');
     return getOptions([...baseArgv, '--config', 'alternate.config.js']);
   });
   expect(config.branch).toEqual('origin/foo');
 });
 
 const writeConfig = async (contents: string) => {
-  await writeFileAsync('beachball.config.js', contents);
+  await fs.writeFile('beachball.config.js', contents);
 };
 
-const inDirectory = async <T>(directory: string, cb: () => T): Promise<T> => {
+const inDirectory = async <T>(directory: string, cb: () => Promise<T>): Promise<T> => {
   const originalDirectory = process.cwd();
   process.chdir(directory);
   try {
--- src/bump/bumpPackageInfoVersion.ts
+++ src/bump/bumpPackageInfoVersion.ts
@@ -22,7 +22,7 @@ export function bumpPackageInfoVersion(pkgName: string, bumpInfo: BumpInfo, opti
     return;
   }
   if (!info.private) {
-    info.version = semver.inc(info.version, changeType, options.prereleasePrefix) as string;
+    info.version = semver.inc(info.version, changeType, options.prereleasePrefix || undefined) as string;
     modifiedPackages.add(pkgName);
   }
 }
