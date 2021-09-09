cc48c1dc5945b9832281340c633058a03a066172
Elizabeth Craig <elcraig@microsoft.com>
2021-09-09 13:05:17 -0700

    Allow prerelease changes to have dependentChangeType: patch

--- src/validation/isValidDependantChangeType.ts
+++ src/validation/isValidDependantChangeType.ts
@@ -2,10 +2,13 @@ import { ChangeType } from '../types/ChangeInfo';
 
 export function isValidDependentChangeType(
   dependentChangeType: ChangeType,
-  disallwedDependentChangeTypes: ChangeType[] | null
+  disallowedChangeTypes: ChangeType[] | null
 ) {
+  // patch is always allowed as a dependentChangeType
+  const disallowedDependentChangeTypes = (disallowedChangeTypes || []).filter(t => t !== 'patch');
+
   return (
     ['patch', 'major', 'minor', 'prerelease', 'none'].includes(dependentChangeType) &&
-    !disallwedDependentChangeTypes?.includes(dependentChangeType)
+    !disallowedDependentChangeTypes.includes(dependentChangeType)
   );
 }
