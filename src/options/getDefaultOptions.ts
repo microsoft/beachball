bb062e1177eed0d56db4cfdc528cfb2a7c0e38a0
Elizabeth Craig <elcraig@microsoft.com>
2022-05-03 20:07:42 -0700

    Remove non-meaningful or outdated default options

--- src/options/getDefaultOptions.ts
+++ src/options/getDefaultOptions.ts
@@ -1,34 +1,20 @@
 import { BeachballOptions } from '../types/BeachballOptions';
 
-export function getDefaultOptions() {
+export function getDefaultOptions(): Partial<BeachballOptions> {
   return {
-    all: false,
     authType: 'authtoken',
-    branch: 'origin/master',
     command: 'change',
-    message: '',
     publish: true,
     bumpDeps: true,
     push: true,
     registry: 'https://registry.npmjs.org/',
-    token: '',
     gitTags: true,
-    tag: '',
-    yes: false,
     access: 'restricted',
-    package: '',
     changehint: 'Run "beachball change" to create a change file',
-    type: null,
     fetch: true,
-    version: false,
-    disallowedChangeTypes: null,
     defaultNpmTag: 'latest',
-    scope: null,
     retries: 3,
-    timeout: undefined,
     bump: true,
-    canaryName: undefined,
     generateChangelog: true,
-    depth: undefined,
-  } as BeachballOptions;
+  };
 }
