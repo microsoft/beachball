4ee03750b37a29764f765bd2c4f8e91e6f8fb6e3
Elizabeth Craig <elcraig@microsoft.com>
2022-08-10 16:16:14 -0700

    simplify test log check

--- src/__e2e__/publishRegistry.test.ts
+++ src/__e2e__/publishRegistry.test.ts
@@ -394,9 +394,7 @@ describe('publish command (registry)', () => {
     });
 
     await expect(publishPromise).rejects.toThrow();
-    expect(
-      logs.mocks.log.mock.calls.some(([arg0]) => typeof arg0 === 'string' && arg0.includes('Retrying... (3/3)'))
-    ).toBeTruthy();
+    expect(logs.mocks.log).toHaveBeenCalledWith(expect.stringMatching('Retrying... (3/3)'));
 
     await registry.start();
   });
