c1f5044a4d7a3f9eb36035b3b02c60ae1ecfba68
Elizabeth Craig <elcraig@microsoft.com>
2022-08-10 17:29:56 -0700

    Make test tmpdir utility return real paths (#741)

--- src/__fixtures__/tmpdir.ts
+++ src/__fixtures__/tmpdir.ts
@@ -1,4 +1,5 @@
 import * as tmp from 'tmp';
+import fs from 'fs-extra';
 
 // tmp is supposed to be able to clean up automatically, but this doesn't always work within jest.
 // So we attempt to use its built-in cleanup mechanisms, but tests should ideally do their own cleanup too.
@@ -7,6 +8,13 @@ import * as tmp from 'tmp';
 tmp.setGracefulCleanup();
 
 export function tmpdir(options: tmp.DirOptions): string {
-  // "unsafe" means delete on exit even if it still contains files...which actually is safe
-  return tmp.dirSync({ ...options, unsafeCleanup: true }).name;
+  // Get the real path because on Mac, tmp will return /var/... which is actually a symlink to /private/var/...
+  // (and mixing the symlink path and the real path causes issues in functions that use relative paths)
+  return fs.realpathSync(
+    tmp.dirSync({
+      ...options,
+      // "Unsafe" means delete on exit even if it still contains files...which actually is safe.
+      unsafeCleanup: true,
+    }).name
+  );
 }
