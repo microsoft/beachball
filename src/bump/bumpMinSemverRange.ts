8757fdce0924175b976ac9f7f47e044cd833dcde
Elizabeth Craig <elcraig@microsoft.com>
2023-05-17 15:13:40 -0700

    more cleanup

--- src/bump/bumpMinSemverRange.ts
+++ src/bump/bumpMinSemverRange.ts
@@ -4,24 +4,28 @@ export function bumpMinSemverRange(minVersion: string, semverRange: string) {
   if (semverRange === '*') {
     return semverRange;
   }
-
-  if (new Set(['workspace:*', 'workspace:~', 'workspace:^']).has(semverRange)) {
+  if (['workspace:*', 'workspace:~', 'workspace:^'].includes(semverRange)) {
     // For basic workspace ranges we can just preserve current value and replace during publish
     // https://pnpm.io/workspaces#workspace-protocol-workspace
     return semverRange;
-  } else if (semverRange.startsWith('~') || semverRange.startsWith('^')) {
+  }
+  if (semverRange[0] === '~' || semverRange[0] === '^') {
     // ~1.0.0
     // ^1.0.0
     return semverRange[0] + minVersion;
-  } else if (semverRange.startsWith('workspace:~') || semverRange.startsWith('workspace:^')) {
+  }
+  if (semverRange.startsWith('workspace:~') || semverRange.startsWith('workspace:^')) {
     // workspace:~1.0.0
     // workspace:^1.0.0
     return `workspace:${semverRange[10]}${minVersion}`;
-  } else if (semverRange.includes('>')) {
-    // Less frequently used, but we treat any of these kinds of ranges to be within a minor band for now: more complex understand of the semver range utility is needed to do more
+  }
+  if (semverRange.includes('>')) {
+    // Less frequently used, but we treat any of these kinds of ranges to be within a minor band for now:
+    // more complex understanding of the semver range utility is needed to do more
     // >=1.0.0 <2.0.0
     return `>=${minVersion} <${semver.inc(minVersion, 'major')}`;
-  } else if (semverRange.includes(' - ')) {
+  }
+  if (semverRange.includes(' - ')) {
     // 1.0.0 - 2.0.0
     return `${minVersion} - ${semver.inc(minVersion, 'major')}`;
   }
--- src/bump/bumpPackageInfoVersion.ts
+++ src/bump/bumpPackageInfoVersion.ts
@@ -9,19 +9,15 @@ export function bumpPackageInfoVersion(pkgName: string, bumpInfo: BumpInfo, opti
   const { calculatedChangeTypes, packageInfos, modifiedPackages } = bumpInfo;
   const info = packageInfos[pkgName];
   const changeType = calculatedChangeTypes[pkgName];
+
   if (!info) {
     console.log(`Unknown package named "${pkgName}" detected from change files, skipping!`);
-    return;
-  }
-  if (changeType === 'none') {
+  } else if (changeType === 'none') {
     console.log(`"${pkgName}" has a "none" change type, no version bump is required.`);
-    return;
-  }
-  if (info.private) {
+  } else if (info.private) {
     console.log(`Skipping bumping private package "${pkgName}"`);
-    return;
-  }
-  if (!info.private) {
+  } else {
+    // Version should be updated
     info.version = semver.inc(
       info.version,
       options.prereleasePrefix ? 'prerelease' : changeType,
