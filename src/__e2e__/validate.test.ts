2a0c4b19dce5774a8bcfb4dcd97a2aab6c52e01c
Elizabeth Craig <elcraig@microsoft.com>
2023-05-27 00:50:39 -0700

    improve test

--- src/__e2e__/validate.test.ts
+++ src/__e2e__/validate.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, afterEach, jest } from '@jest/globals';
+import { describe, expect, it, afterAll, jest, beforeAll, afterEach } from '@jest/globals';
 import { defaultBranchName } from '../__fixtures__/gitDefaults';
 import { RepositoryFactory } from '../__fixtures__/repositoryFactory';
 import { BeachballOptions } from '../types/BeachballOptions';
@@ -6,43 +6,54 @@ import { initMockLogs } from '../__fixtures__/mockLogs';
 import { validate } from '../validation/validate';
 
 describe('validate', () => {
-  let repositoryFactory: RepositoryFactory | undefined;
+  let repositoryFactory: RepositoryFactory;
   const logs = initMockLogs();
   // this is mocked in jestSetup
   const processExit = process.exit as jest.MockedFunction<typeof process.exit>;
-  const processExitImpl = processExit.getMockImplementation()!;
 
-  afterEach(() => {
-    repositoryFactory?.cleanUp();
-    repositoryFactory = undefined;
-    processExit.mockImplementation(processExitImpl);
+  beforeAll(() => {
+    // these tests can reuse a factory because they don't push changes
+    repositoryFactory = new RepositoryFactory('monorepo');
   });
 
-  // Some super basic tests for now to ensure that validate() doesn't get blatantly broken again
-  it('succeeds in a basic scenario', () => {
-    repositoryFactory = new RepositoryFactory('monorepo');
+  afterEach(() => {
+    processExit.mockClear();
+  });
+
+  afterAll(() => {
+    repositoryFactory.cleanUp();
+  });
+
+  it('succeeds with no changes', () => {
     const repo = repositoryFactory.cloneRepository();
     repo.checkout('-b', 'test');
     const options = { path: repo.rootPath, branch: defaultBranchName } as BeachballOptions;
+
     const result = validate(options);
     expect(result.isChangeNeeded).toBe(false);
     expect(logs.mocks.error).not.toHaveBeenCalled();
-    // the success log for the "change" command is done in the main cli file, not validate()
+    // the success log for the "check" command is done in the main cli file, not validate()
   });
 
-  it('fails if change files are needed', () => {
-    // this calls process.exit on failure, so mock it
-    processExit.mockImplementation((() => {
-      /*no-op*/
-    }) as any);
-
-    repositoryFactory = new RepositoryFactory('monorepo');
+  it('exits with error by default if change files are needed', () => {
     const repo = repositoryFactory.cloneRepository();
     repo.checkout('-b', 'test');
     repo.stageChange('packages/foo/test.js');
     const options = { path: repo.rootPath, branch: defaultBranchName } as BeachballOptions;
-    const result = validate(options);
-    expect(result.isChangeNeeded).toBe(true);
+
+    expect(() => validate(options)).toThrowError(/process\.exit/);
+    expect(processExit).toHaveBeenCalledWith(1);
     expect(logs.mocks.error).toHaveBeenCalledWith('ERROR: Change files are needed!');
+  });
+
+  it('returns an error if change files are needed and allowMissingChangeFiles is true', () => {
+    const repo = repositoryFactory.cloneRepository();
+    repo.checkout('-b', 'test');
+    repo.stageChange('packages/foo/test.js');
+    const options = { path: repo.rootPath, branch: defaultBranchName } as BeachballOptions;
+
+    const result = validate(options, { allowMissingChangeFiles: true });
+    expect(result.isChangeNeeded).toBe(true);
+    expect(logs.mocks.error).not.toHaveBeenCalled();
   });
 });
