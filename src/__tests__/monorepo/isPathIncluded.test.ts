97ac186c666dce1b2a48b6d6bf4e272e2133d833
Elizabeth Craig <elcraig@microsoft.com>
2023-08-31 19:58:22 -0700

    Minor improvements to path include/exclude logic

--- src/__tests__/monorepo/isPathIncluded.test.ts
+++ src/__tests__/monorepo/isPathIncluded.test.ts
@@ -2,23 +2,35 @@ import { describe, expect, it } from '@jest/globals';
 import { isPathIncluded } from '../../monorepo/isPathIncluded';
 
 describe('isPathIncluded', () => {
-  it('should return true if path is included with single include path', () => {
+  it('returns true if path is included (single include path)', () => {
     expect(isPathIncluded('packages/a', 'packages/*')).toBeTruthy();
   });
 
-  it('should return false if path is excluded with single exclude path', () => {
+  it('returns false if path is excluded (single exclude path)', () => {
     expect(isPathIncluded('packages/a', 'packages/*', '!packages/a')).toBeFalsy();
   });
 
-  it('should return true if path is included with multiple include paths', () => {
+  it('returns true if path is included (multiple include paths)', () => {
     expect(isPathIncluded('packages/a', ['packages/b', 'packages/a'], ['!packages/b'])).toBeTruthy();
   });
 
-  it('should return false if path is excluded with multiple exclude paths', () => {
-    expect(isPathIncluded('packages/a', ['packages/*'], ['!packages/a'])).toBeFalsy();
+  it('returns false if path is excluded (multiple exclude paths)', () => {
+    expect(isPathIncluded('packages/a', ['packages/*'], ['!packages/a', '!packages/b'])).toBeFalsy();
   });
 
-  it('should return false if include path is empty', () => {
+  it('returns true if include is true (no exclude paths)', () => {
+    expect(isPathIncluded('packages/a', true)).toBeTruthy();
+  });
+
+  it('returns false if include is true and path is excluded', () => {
+    expect(isPathIncluded('packages/a', true, '!packages/a')).toBeFalsy();
+  });
+
+  it('returns false if include path is empty', () => {
     expect(isPathIncluded('packages/a', '')).toBeFalsy();
+  });
+
+  it('ignores empty exclude path array', () => {
+    expect(isPathIncluded('packages/a', 'packages/*', [])).toBeTruthy();
   });
 });
--- src/monorepo/isPathIncluded.ts
+++ src/monorepo/isPathIncluded.ts
@@ -2,20 +2,34 @@ import minimatch from 'minimatch';
 
 /**
  * Check if a relative path should be included given include and exclude patterns using minimatch.
+ * @param relativePath Relative path to check.
+ * @param include Include pattern(s). If `true`, include all paths except those excluded.
+ * @param exclude Exclude pattern(s). Currently these must be **negated** patterns:
+ * e.g. if you want to exclude `packages/foo`, you must specify `exclude` as `!packages/foo`.
+ * (This will be fixed in a future major version.)
  */
-export function isPathIncluded(relativePath: string, include: string | string[], exclude?: string | string[]): boolean {
-  const includePatterns = typeof include === 'string' ? [include] : include;
-  let shouldInclude = includePatterns.reduce(
-    (included, pattern) => included || minimatch(relativePath, pattern),
-    false
-  );
+export function isPathIncluded(
+  relativePath: string,
+  include: string | string[] | true,
+  exclude?: string | string[]
+): boolean {
+  let shouldInclude: boolean;
+  if (include === true) {
+    shouldInclude = true;
+  } else {
+    const includePatterns = typeof include === 'string' ? [include] : include;
+    shouldInclude = includePatterns.some(pattern => minimatch(relativePath, pattern));
+  }
 
-  if (exclude) {
+  if (exclude?.length && shouldInclude) {
+    // TODO: this is weird/buggy--it assumes that exclude patterns are always negated,
+    // which intuitively (or comparing to other tools) is not how it should work.
+    // If this is fixed, updates will be needed in:
+    // - getScopedPackages()
+    // - ChangelogGroupOptions
+    // - VersionGroupOptions
     const excludePatterns = typeof exclude === 'string' ? [exclude] : exclude;
-    shouldInclude = excludePatterns.reduce(
-      (excluded: boolean, pattern: string) => excluded && minimatch(relativePath, pattern),
-      shouldInclude
-    );
+    shouldInclude = excludePatterns.every(pattern => minimatch(relativePath, pattern));
   }
 
   return shouldInclude;
