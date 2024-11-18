3addf0f5f5299091aea7ab3fa427e4a54940a1cd
Elizabeth Craig <elcraig@microsoft.com>
2024-11-18 07:53:29 -0800

    WIP recursive merge package options, needs tests

--- src/options/getPackageOptions.ts
+++ src/options/getPackageOptions.ts
@@ -1,5 +1,5 @@
 import { cosmiconfigSync } from 'cosmiconfig';
-import { PackageOptions } from '../types/BeachballOptions';
+import { CliOptions, PackageOptions, RepoOptions } from '../types/BeachballOptions';
 import { getCliOptions } from './getCliOptions';
 import { getRepoOptions } from './getRepoOptions';
 import { getDefaultOptions } from './getDefaultOptions';
@@ -10,17 +10,20 @@ import { env } from '../env';
  * Gets all package level options (default + root options + package options + cli options)
  * This function inherits packageOptions from the repoOptions
  */
-export function getCombinedPackageOptions(actualPackageOptions: Partial<PackageOptions>): PackageOptions {
+export function getCombinedPackageOptions(
+  actualPackageOptions: Partial<PackageOptions>,
+  testOptions?: {
+    cliOptions?: CliOptions;
+    repoOptions?: RepoOptions;
+  }
+): PackageOptions {
   const defaultOptions = getDefaultOptions();
   // Don't use options from process.argv or the beachball repo in tests
-  const cliOptions = !env.isJest && getCliOptions(process.argv);
-  const repoOptions = cliOptions && getRepoOptions(cliOptions);
-  return {
-    ...defaultOptions,
-    ...repoOptions,
-    ...actualPackageOptions,
-    ...cliOptions,
-  };
+  const cliOptions = testOptions?.cliOptions || (!env.isJest ? getCliOptions(process.argv) : undefined);
+  const repoOptions = cliOptions?.path ? getRepoOptions(cliOptions) : undefined;
+
+  // Repo options and package options could potentially have nested objects.
+  return mergeObjects(defaultOptions, repoOptions, actualPackageOptions, cliOptions) as PackageOptions;
 }
 
 /**
@@ -35,4 +38,24 @@ export function getPackageOptions(packagePath: string): Partial<PackageOptions> 
     // File does not exist, returns the default packageOptions
     return {};
   }
+}
+
+function mergeObjects(...objects: (Record<string, any> | undefined)[]) {
+  const acc: Record<string, any> = {};
+
+  for (const obj of objects) {
+    if (!obj || typeof obj !== 'object') {
+      continue;
+    }
+
+    for (const [key, val] of Object.entries(obj)) {
+      if (val && typeof val === 'object' && !Array.isArray(val)) {
+        acc[key] = mergeObjects([acc[key] || {}, val]);
+      } else {
+        acc[key] = val;
+      }
+    }
+  }
+
+  return acc;
 }
