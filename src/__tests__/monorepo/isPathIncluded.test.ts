25b444f79983c85f4f5962b3193af122a38dae00
Elizabeth Craig <elcraig@microsoft.com>
2024-11-24 07:10:52 -0800

    [breaking] Fix behavior of VersionGroupOptions.exclude (see #916)

--- src/__tests__/monorepo/isPathIncluded.test.ts
+++ src/__tests__/monorepo/isPathIncluded.test.ts
@@ -3,34 +3,43 @@ import { isPathIncluded } from '../../monorepo/isPathIncluded';
 
 describe('isPathIncluded', () => {
   it('returns true if path is included (single include path)', () => {
-    expect(isPathIncluded('packages/a', 'packages/*')).toBeTruthy();
+    expect(isPathIncluded({ relativePath: 'packages/a', include: 'packages/*' })).toBeTruthy();
   });
 
-  it('returns false if path is excluded (single exclude path)', () => {
-    expect(isPathIncluded('packages/a', 'packages/*', '!packages/a')).toBeFalsy();
+  it('returns false if path is not included, with single include path', () => {
+    expect(isPathIncluded({ relativePath: 'stuff/b', include: 'packages/*' })).toBeFalsy();
+    expect(isPathIncluded({ relativePath: 'packages/b', include: 'packages/!(b)' })).toBeFalsy();
   });
 
-  it('returns true if path is included (multiple include paths)', () => {
-    expect(isPathIncluded('packages/a', ['packages/b', 'packages/a'], ['!packages/b'])).toBeTruthy();
+  it('returns false if path is excluded, with single exclude path', () => {
+    expect(isPathIncluded({ relativePath: 'packages/a', include: 'packages/*', exclude: 'packages/a' })).toBeFalsy();
   });
 
-  it('returns false if path is excluded (multiple exclude paths)', () => {
-    expect(isPathIncluded('packages/a', ['packages/*'], ['!packages/a', '!packages/b'])).toBeFalsy();
+  it('returns true if path is included, with multiple include paths', () => {
+    expect(
+      isPathIncluded({ relativePath: 'packages/a', include: ['packages/b', 'packages/a'], exclude: ['packages/b'] })
+    ).toBeTruthy();
+  });
+
+  it('returns false if path is excluded, with multiple exclude paths', () => {
+    expect(
+      isPathIncluded({ relativePath: 'packages/a', include: ['packages/*'], exclude: ['packages/a'] })
+    ).toBeFalsy();
   });
 
   it('returns true if include is true (no exclude paths)', () => {
-    expect(isPathIncluded('packages/a', true)).toBeTruthy();
+    expect(isPathIncluded({ relativePath: 'packages/a', include: true })).toBeTruthy();
   });
 
   it('returns false if include is true and path is excluded', () => {
-    expect(isPathIncluded('packages/a', true, '!packages/a')).toBeFalsy();
+    expect(isPathIncluded({ relativePath: 'packages/a', include: true, exclude: 'packages/a' })).toBeFalsy();
   });
 
   it('returns false if include path is empty', () => {
-    expect(isPathIncluded('packages/a', '')).toBeFalsy();
+    expect(isPathIncluded({ relativePath: 'packages/a', include: '' })).toBeFalsy();
   });
 
   it('ignores empty exclude path array', () => {
-    expect(isPathIncluded('packages/a', 'packages/*', [])).toBeTruthy();
+    expect(isPathIncluded({ relativePath: 'packages/a', include: 'packages/*', exclude: [] })).toBeTruthy();
   });
 });
