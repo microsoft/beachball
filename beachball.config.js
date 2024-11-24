ffeddcffd7ec76a9f8a813847219b7750996bd7d
Elizabeth Craig <elcraig@microsoft.com>
2024-11-23 22:39:23 -0800

    [breaking] Use node 20 and yarn 4 (and remove docs pseudo-package)

--- beachball.config.js
+++ beachball.config.js
@@ -1,12 +1,9 @@
 // @ts-check
 /** @type {import('./src/types/BeachballOptions').RepoOptions}*/
 module.exports = {
-  disallowedChangeTypes: ['major'],
   ignorePatterns: [
-    '.*ignore',
-    '.github/**',
-    '.prettierrc.json5',
-    '.vscode/**',
+    '.*',
+    '.*/**',
     'docs/**',
     'docs/.vuepress/**',
     'jest.*.js',
@@ -16,4 +13,11 @@ module.exports = {
     // This one is especially important (otherwise dependabot would be blocked by change file requirements)
     'yarn.lock',
   ],
+
+  // TODO (release): change back to 'major'
+  disallowedChangeTypes: ['prerelease', 'premajor', 'preminor', 'prepatch'],
+  // TODO (release): remove these
+  branch: 'next',
+  defaultNpmTag: 'next',
+  gitTags: false,
 };
