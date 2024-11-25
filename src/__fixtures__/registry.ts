2fc7c985dea03216b7781580e3b537bb98a569d9
Elizabeth Craig <elcraig@microsoft.com>
2024-11-24 20:23:06 -0800

    Update verdaccio for tests (#1011)

--- src/__fixtures__/registry.ts
+++ src/__fixtures__/registry.ts
@@ -1,10 +1,15 @@
+import { ConfigBuilder } from '@verdaccio/config';
+import { fork, type ChildProcess } from 'child_process';
 import execa from 'execa';
+import fs from 'fs-extra';
+import getPort from 'get-port';
 import path from 'path';
-// @ts-ignore
-import fp from 'find-free-port';
-import verdaccioUser from './verdaccioUser';
+import { tmpdir } from './tmpdir';
 
-const verdaccioApi = require.resolve('./verdaccio.js');
+const verdaccioUser = {
+  username: 'fake',
+  password: 'fake',
+};
 
 /** Range of ports tried (increase this if the tests are failing due to ports unavailable) */
 const portRange = 1000;
@@ -19,10 +24,11 @@ const knownTests = ['packagePublish', 'publishE2E', 'publishRegistry', 'syncE2E'
 //       A value scaled with the number of test modules using Registry should work, starting with 20 seconds or so.
 
 export class Registry {
-  private server?: execa.ExecaChildProcess = undefined;
+  private server?: ChildProcess = undefined;
   private port?: number = undefined;
   private startPort: number;
   private testName: string;
+  private tempRoot: string | undefined;
 
   constructor(filename: string) {
     this.testName = path.basename(filename, '.test.ts');
@@ -32,7 +38,7 @@ export class Registry {
     this.startPort = 4873 + knownTests.indexOf(this.testName) * portRange;
   }
 
-  async start() {
+  public async start(): Promise<unknown> {
     if (this.server) {
       throw new Error('Server already started');
     }
@@ -46,7 +52,7 @@ export class Registry {
     // If this is consistently having problems, probably it's best to increase portRange.
     const maxPort = this.startPort + portRange;
     console.log(`Looking for free ports in range ${this.startPort} to ${maxPort}`);
-    const tryPort = await fp(this.startPort, maxPort);
+    const tryPort = await getPort({ port: getPort.makeRange(this.startPort, maxPort) });
 
     // Try to start the server. If it fails, it's likely a config error or something where a retry
     // won't be helpful, so just let it throw.
@@ -59,14 +65,14 @@ export class Registry {
       console.log(`logging in to ${registry}`);
       const npm = execa('npm', ['adduser', '--registry', registry]);
       // for some reason there's no way to supply the username, password, and email besides stdin
-      npm.stdout!.on('data', chunk => {
-        chunk = String(chunk);
-        if (chunk.includes('Username:')) {
-          npm.stdin!.write(verdaccioUser.username + '\r\n');
-        } else if (chunk.includes('Password:')) {
-          npm.stdin!.write(verdaccioUser.password + '\r\n');
-        } else if (chunk.includes('Email:')) {
-          npm.stdin!.write('fake@example.com\r\n');
+      npm.stdout?.on('data', chunk => {
+        const chunkStr = String(chunk);
+        if (chunkStr.includes('Username:')) {
+          npm.stdin?.write(verdaccioUser.username + '\r\n');
+        } else if (chunkStr.includes('Password:')) {
+          npm.stdin?.write(verdaccioUser.password + '\r\n');
+        } else if (chunkStr.includes('Email:')) {
+          npm.stdin?.write('fake@example.com\r\n');
         }
       });
       await npm;
@@ -78,41 +84,66 @@ export class Registry {
     }
   }
 
-  private async startWithPort(port: number) {
+  /** Delete the temp directory used for the config file. */
+  public cleanUp(): void {
+    try {
+      this.tempRoot && fs.removeSync(this.tempRoot);
+      this.tempRoot = undefined;
+    } catch {
+      // This can fail on Windows with EBUSY (likely due to the server not being fully shut down
+      // or all handles released or something). Just ignore it.
+    }
+  }
+
+  private async startWithPort(port: number): Promise<void> {
+    this.tempRoot ??= tmpdir({ prefix: 'beachball-registry-' });
+    const configName = 'config.yaml';
+    const configPath = path.join(this.tempRoot, configName);
+    if (!fs.existsSync(configPath)) {
+      this.writeConfig(configPath);
+    }
+
     return new Promise((resolve, reject) => {
+      let hasReturned = false;
+      const rejectWrapper = (err: unknown) => {
+        // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+        !hasReturned && reject(err);
+        hasReturned = true;
+      };
+
       try {
-        // set VERDACCIO_LOG env to write a log file
-        const logPath = process.env.VERDACCIO_LOG ? `verdaccio-${Date.now()}.log` : '';
-        this.server = execa(process.execPath, [verdaccioApi, port.toString(), logPath]);
+        const verdaccioBin = require.resolve('verdaccio/bin/verdaccio');
+        this.server = fork(verdaccioBin, ['--listen', String(port), '--config', `./${configName}`], {
+          cwd: this.tempRoot,
+          stdio: 'pipe',
+        });
+
+        this.server.on('message', (msg: { verdaccio_started: boolean }) => {
+          if (msg.verdaccio_started) {
+            hasReturned = true;
+            resolve();
+          } else {
+            rejectWrapper(new Error(`unexpected message from verdaccio: ${JSON.stringify(msg)}`));
+          }
+        });
+
+        this.server.stderr?.on('data', data => {
+          const dataStr = String(data);
+          if (!dataStr.includes('Debugger attached')) {
+            rejectWrapper(new Error(dataStr));
+          }
+        });
+
+        this.server.on('error', error => {
+          rejectWrapper(error);
+        });
       } catch (err) {
-        return reject(err);
+        rejectWrapper(err);
       }
-
-      if (!this.server || !this.server.stdout || !this.server.stderr) {
-        return reject(new Error('server not initialized correctly'));
-      }
-
-      this.server.stdout.on('data', data => {
-        if (data.includes('verdaccio running')) {
-          console.log(`Started registry for ${this.testName} on port ${port}`);
-          resolve(port);
-        }
-      });
-
-      this.server.stderr.on('data', data => {
-        const dataStr = String(data);
-        if (!dataStr.includes('Debugger attached')) {
-          reject(new Error(dataStr));
-        }
-      });
-
-      this.server.on('error', data => {
-        reject(new Error(String(data)));
-      });
     });
   }
 
-  stop() {
+  stop(): void {
     if (this.server) {
       this.server.kill();
       this.server = undefined;
@@ -122,7 +153,7 @@ export class Registry {
   /**
    * Reset the state of the registry to an empty registry. Starts server if not already started.
    */
-  async reset() {
+  public async reset(): Promise<void> {
     // Since we're running in memory we can just restart the server.
     this.stop();
     await this.start();
@@ -131,10 +162,48 @@ export class Registry {
   /**
    * A helper to get registry URL based on currently used port.
    */
-  getUrl() {
+  public getUrl(): string {
     if (!this.port) {
       throw new Error(`Can't getRegistryUrl, no valid port assigned.`);
     }
     return `http://localhost:${this.port}`;
+  }
+
+  /** Write the verdaccio config to the temp directory */
+  private writeConfig(configPath: string): void {
+    const configBuilder = ConfigBuilder.build({
+      // Something about npm 8 makes publishing fail with anonymous access--from debugging, it might be trying
+      // to read the package from the registry before publishing it, and verdaccio doesn't handle that well.
+      // To work around this, add fake user info, which is also used in registry.ts to authenticate.
+      auth: {
+        // This uses verdaccio-auth-memory
+        'auth-memory': {
+          users: { fake: verdaccioUser },
+        },
+      },
+      // This is the old anonymous access config--it still works for accessing packages, but not for publishing
+      packages: {
+        '**': {
+          access: ['$anonymous'],
+          publish: ['$anonymous'],
+        },
+      },
+      store: {
+        // This uses verdaccio-memory
+        memory: { limit: 1000 },
+      },
+    });
+
+    // set VERDACCIO_LOG env to write a log file
+    if (process.env.VERDACCIO_LOG) {
+      configBuilder.addLogger({
+        type: 'file',
+        level: 'trace',
+        format: 'file',
+        path: path.join(process.cwd(), `verdaccio-${Date.now()}.log`),
+      });
+    }
+
+    fs.writeFileSync(configPath, configBuilder.getAsYaml());
   }
 }
