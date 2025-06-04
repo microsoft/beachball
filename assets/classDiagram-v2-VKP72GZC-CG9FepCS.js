1dafd1d831dbe6fbcc9007514a625d5e6b2823e4
Ken Chau <kchau@microsoft.com>
2025-06-04 08:03:34 +0000

    Updates

--- assets/classDiagram-v2-VKP72GZC-CG9FepCS.js
+++
@@ -1,1 +0,0 @@
-import{w as a,Y as s,k as t,u as e}from"./chunk-D5WH42HA-BtUooLmy.js";import"./chunk-BOXWCURE-jvu_YiT3.js";import{m as o}from"./mermaid.esm.min-DJSe_cTO.js";import"./app-CPNLVqzj.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
