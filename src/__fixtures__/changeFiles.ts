55f2e300feb3e1711ff2df66ae2bced69efc2282
Elizabeth Craig <elcraig@microsoft.com>
2026-03-11 19:38:50 -0700

    helpers

--- src/__fixtures__/changeFiles.ts
+++ src/__fixtures__/changeFiles.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { writeChangeFiles } from '../changefile/writeChangeFiles';
 import { getChangePath } from '../paths';
-import type { ChangeFileInfo, ChangeType } from '../types/ChangeInfo';
+import type { ChangeFileInfo, ChangeSet, ChangeType } from '../types/ChangeInfo';
 import type { BeachballOptions } from '../types/BeachballOptions';
 
 /** Change file with `packageName` required and other props optional */
@@ -13,6 +13,11 @@ export const fakeEmail = 'test@test.com';
 
 /**
  * Generate a change file for the given package.
+ * Default values:
+ * - `type: 'minor'`
+ * - `dependentChangeType: 'patch'`
+ * - `comment: '<packageName> comment'`
+ * - `email: 'test@test.com'`
  */
 export function getChange(
   packageName: string,
@@ -29,18 +34,11 @@ export function getChange(
 }
 
 /**
- * Generates and writes change files for the given packages.
- * Also commits if `options.commit` is true (the default with full options) and the context is a git repo.
+ * Generates change files for the given packages.
  * @param changes Array of package names or partial change files (which must include `packageName`).
- * Default values:
- * - `type: 'minor'`
- * - `dependentChangeType: 'patch'`
- * - `comment: '<packageName> comment'`
- * - `email: 'test@test.com'`
- *
- * @param changes Array of package names or partial change files (which must include `packageName`).
+ * See {@link getChange} for default values.
  */
-export function generateChanges(changes: (string | PartialChangeFile)[]): ChangeFileInfo[] {
+function generateChanges(changes: (string | PartialChangeFile)[]): ChangeFileInfo[] {
   return changes.map(change => {
     change = typeof change === 'string' ? { packageName: change } : change;
     return {
@@ -48,6 +46,18 @@ export function generateChanges(changes: (string | PartialChangeFile)[]): Change
       ...change,
     };
   });
+}
+
+/**
+ * Generates a change set for the given packages (the file names will not be realistic).
+ * @param changes Array of package names or partial change files (which must include `packageName`).
+ * See {@link getChange} for default values.
+ */
+export function generateChangeSet(changes: (string | PartialChangeFile)[]): ChangeSet {
+  return generateChanges(changes).map((change, i) => ({
+    change,
+    changeFile: `change${i}.json`,
+  }));
 }
 
 /**
