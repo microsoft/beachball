a17cd607ba79fd0dd282c948be08c58cb91b2809
Elizabeth Craig <elcraig@microsoft.com>
2026-04-17 17:56:07 -0700

    More monorepo updates

--- beachball.config.js
+++ beachball.config.js
@@ -1,9 +1,10 @@
 // @ts-check
-/** @type {Partial<import('./src/types/BeachballOptions').RepoOptions>}*/
+/** @type {Partial<import('./packages/beachball/src/types/BeachballOptions').RepoOptions>}*/
 const config = {
   branch: 'main',
   commit: false,
   disallowedChangeTypes: ['major'],
+  groupChanges: true,
   ignorePatterns: ['.*ignore', '.eslintrc.js', 'jest.*.js', 'src/__*/**'],
 };
 
--- packages/beachball/jest.config.js
+++ packages/beachball/jest.config.js
@@ -1,49 +1,23 @@
 // @ts-check
-const path = require('path');
 
-/** @type {import('@jest/types').Config.InitialProjectOptions} */
-const commonOptions = {
-  injectGlobals: false,
-  roots: ['<rootDir>/src'],
-  setupFilesAfterEnv: [path.resolve(__dirname, '../../scripts/jestSetup.js')],
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      // in ts-jest, this means skip type checking (we already type check in the build step)
-      { isolatedModules: true },
-    ],
-  },
-  testEnvironment: 'node',
-};
+const { getConfigWithProjects } = require('@microsoft/beachball-scripts/config/jest');
 
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  reporters: ['default', 'github-actions'],
-  testTimeout: 60000,
-  // Enable to locally test with coverage info (will only work properly with `yarn test`, not
-  // `test:all` or individual projects). This would be tricky to enable in CI due to the multiple
-  // test projects that run in sequence. Coverage also doesn't alone capture tricky scenarios.
-  // collectCoverage: true,
-  coveragePathIgnorePatterns: ['/node_modules/', '__fixtures__'],
-  coverageThreshold: {
-    global: { branches: 80, functions: 100, lines: 90, statements: 90 },
-  },
-  projects: [
+module.exports = getConfigWithProjects(
+  [
     {
       displayName: 'unit',
       testMatch: ['**/__(tests|fixtures)__/**/*.test.ts'],
-      ...commonOptions,
     },
     {
       displayName: 'functional',
       testMatch: ['**/__functional__/**/*.test.ts'],
-      ...commonOptions,
     },
     {
       displayName: 'e2e',
       testMatch: ['**/__e2e__/**/*.test.ts'],
-      ...commonOptions,
     },
   ],
-};
-module.exports = config;
+  {
+    testTimeout: 60000,
+  }
+);
