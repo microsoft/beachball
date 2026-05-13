091220db07edb1c05af2cb414eee611c06f3facc
Elizabeth Craig <elcraig@microsoft.com>
2026-05-13 16:39:42 -0700

    jws bigint

--- packages/esrp-npm-release/src/utils/generateJwsToken.ts
+++ packages/esrp-npm-release/src/utils/generateJwsToken.ts
@@ -12,12 +12,14 @@ export interface JwsTokenParams {
 export function generateJwsToken(params: JwsTokenParams & { releaseRequest: ReleaseRequestMessage }): string {
   const { releaseRequest, certificates, privateKey } = params;
 
+  const expTicks = (BigInt(Date.now()) + 6n * 60n * 1000n) * 10000n + 621355968000000000n;
+
   // Create header with properly typed properties, then override x5c with the non-standard string format
   const header: jws.Header = {
     alg: 'RS256',
     crit: ['exp', 'x5t'],
     // Release service uses .NET ticks, not milliseconds (https://stackoverflow.com/a/7968483)
-    exp: (Date.now() + 6 * 60 * 1000) * 10000 + 621355968000000000,
+    exp: expTicks,
     // Release service uses hex format for thumbprint
     x5t: getThumbprint(certificates[0], 'sha1').toString('hex'),
   };
