6717f573117680f02bf6b1c7f953081ae1584170
Elizabeth Craig <elcraig@microsoft.com>
2026-07-01 21:10:08 -0700

    fix registry

--- beachball.config.js
+++ beachball.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 const { getGitTag, postbumpHook } = require('./scripts/beachballConfigHelpers.cjs');
+const { getPublishRegistry } = require('./scripts/preparePublishRegistry.ts');
 
 /** @type {Partial<import('./packages/beachball/src/types/BeachballOptions').RepoOptions>}*/
 const config = {
@@ -8,6 +9,8 @@ const config = {
   branch: 'main',
   commit: false,
   ignorePatterns: ['.*ignore', '.eslintrc.js', 'eslint.config.*', 'jest.*.js', '**/__*/**'],
+  // Currently, only the publish build runs in ADO
+  registry: process.env.TF_BUILD ? getPublishRegistry() : 'https://registry.npmjs.org/',
 
   // TODO (release): re-enable -- it has to be disabled while releasing actions separately
   groupChanges: false,
