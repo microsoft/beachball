c56934f2ef961d13607d6b3df1c6755a540f68f5
Elizabeth Craig <elcraig@microsoft.com>
2026-07-22 15:37:51 -0700

    updates

--- scripts/bundleNode.ts
+++ scripts/bundleNode.ts
@@ -8,14 +8,16 @@ if (!packageRoot) {
   console.error('Unable to find package root from', process.cwd());
   process.exit(1);
 }
-// Don't use .mjs extension on actions (mainly to avoid touching a generated file and re-triggering
-// a bunch of meaningless alerts).
+// .mjs extensions are unnecessary for actions since they're handled directly by github.
 // Use an explicit .mjs extension on the other bundles since they may be copied.
 const useMjs = !fs.existsSync(path.join(packageRoot, 'action.yaml'));
 
 // entry: relative path to entry file
 // out: extensionless name of output file (relative to dist)
 const [entry = 'src/index.ts', out = 'index'] = process.argv.slice(2);
+
+/** additional permissive licenses not currently included in cloudpack's check */
+const extraLicenses: Array<string | null> = ['BlueOak-1.0.0'];
 
 await bundleNode({
   cwd: packageRoot,
@@ -30,7 +32,7 @@ await bundleNode({
     // ensure files can be copy-pasted
     splitting: false,
   },
-  unacceptableLicenseTest,
+  unacceptableLicenseTest: license => unacceptableLicenseTest(license) && !extraLicenses.includes(license),
   excludeFromNotice: dep => dep.name.startsWith('@azure/') && dep.license === 'MIT',
 }).catch(err => {
   if (!(err instanceof BundleError && err.alreadyLogged)) {
